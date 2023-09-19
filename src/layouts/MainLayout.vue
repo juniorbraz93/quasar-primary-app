7cfd69689efe813876884a75c18547774df12203
Júnior Braz <junior@ar-online.com.br>
2023-09-19 16:40:26 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="flex justify-between items-center q-pa-md">
+      <q-icon name="keyboard_arrow_left" size="25px" />
+      <q-toolbar-title class="text-center"> Frase do Dia </q-toolbar-title>
+
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="leftDrawerOpen = !leftDrawerOpen"
+      />
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Mater learning Project
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksData = [
+  {
+    title: "Inicio",
+    // caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Cursos",
+    // caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+];
+
+export default {
+  name: "MainLayout",
+  components: {
+    EssentialLink,
+  },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData,
+    };
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+
+.q-header{
+  background-color: #f95179;
+
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-page class="container flex column flex-center justify-around">
+    <h4 class="title q-ma-sm text-center">
+      <strong>{{ phrases[index] }}</strong>
+    </h4>
+
+    <q-img
+      src="../assets/paisagem.jpeg"
+      spinner-color="white"
+      style="height: auto; max-width: 260px; border-radius: 10px"
+    />
+
+    <div class="flex justify-around full-width">
+      <q-btn
+        class="btn"
+        outline
+        rounded
+        @click="randomPhrase"
+        label="Gerar Frase"
+      />
+
+      <q-btn class="btn" rounded @click="randomPhrase" label="Gerar Frase" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PharseOfDay",
+  data() {
+    return {
+      phrases: [
+        "Enfrente os problemas e leve a melhor!",
+        "Respire fundo e fique atento às surpresas deliciosas que a vida preparou para você!",
+        "Levanta, sacode a poeira, dá a volta por cima.",
+        "Nem todos os dias são bons, mas há algo bom em cada dia.",
+        "Não se exija demais",
+        "Respeite sua mente e trate seu corpo com carinho.",
+      ],
+      index: 0,
+    };
+  },
+  methods: {
+    randomPhrase() {
+      let index = parseInt(Math.random() * 6 + 0);
+      this.index = index;
+    },
+  },
+  beforeMount() {
+    this.randomPhrase();
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.container{
+  background-color: #f95179;
+}
+
+.title{
+  color: #fff;
+}
+
+.btn{
+  text-align: 'center';
+  background-color: #262525;
+  color: #fff;
+  height: 60px;
+}
+</style>
